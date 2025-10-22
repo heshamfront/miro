53cfbe96790f165d584e1cfdb765a31b803fc106
heshamfront <heshamelkela4@gmail.com>
2025-10-22 06:57:00 +0200

    addition some things

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ console.log('hello world');
   const tabsWrapper = document.querySelector('.tabs-wrapper');
   document.getElementById('scrollLeft').onclick = () => tabsWrapper.scrollBy({ left: -200, behavior: 'smooth' });
   document.getElementById('scrollRight').onclick = () => tabsWrapper.scrollBy({ left: 200, behavior: 'smooth' });
+
+//  start popup image viewer code
