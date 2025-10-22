0bcdecf77e84499386673f523d9515af53a498b2
heshamfront <heshamelkela4@gmail.com>
2025-10-22 04:31:32 +0200

    miro its just inishialize

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log('hello world');
+  const tabsWrapper = document.querySelector('.tabs-wrapper');
+  document.getElementById('scrollLeft').onclick = () => tabsWrapper.scrollBy({ left: -200, behavior: 'smooth' });
+  document.getElementById('scrollRight').onclick = () => tabsWrapper.scrollBy({ left: 200, behavior: 'smooth' });
